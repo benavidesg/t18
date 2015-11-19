6c12bdebf6a4872d61ae2ff46de19c91395a8e6e
Jose Eduardo Benavides Gonzalez <a272682@uach.mx>
2015-11-19 15:06:18 -0700

    Update 18t.js

--- js/18t.js
+++ js/18t.js
@@ -14,31 +14,12 @@ url: "https://andreihelo-restful-api.herokuapp.com/students/",
 data: {id: idRegistro, registration_number: idRegistro, name:firstName, last_name:lastName, status:statusCode },
 dataType: 'json',
 error: function(){alert("Lo siento, no se ha podido registrar a la persona con ID " + idRegistro + ".\n\n\t\t\tPor favor, intente de nuevo.");},
-success: function(){
-
-
-
-
-$.ajax({
-
-url: "https://andreihelo-restful-api.herokuapp.com/students",
-type: 'GET',
-dataType: 'json',
 success: function(data){
-  $.each(data, function(index, item) {
-    $("table tbody").append(
-
-      "<tr><td>" + item.id +"</td><td>"+ item.registration_number +
-      "</td><td>" + item.name + "</td><td>" + item.last_name + "</td><td>" + item.status + "</td></tr>"
-
-//hacer otro documento html aside index, que cuando se cargue
-
-    );
+  alert("Subida Exitosa!");
 
 
-  });
-      }
-    });
+  $("table tbody").append(  "<tr><td>" + data.id +"</td><td>"+ data.registration_number +
+    "</td><td>" + data.name + "</td><td>" + data.last_name + "</td><td>" + data.status + "</td></tr>");
 
 
 
